9d51a5e649507f5197020a21f43cde43aa0063c8
shihjian <31888231+shihjian@users.noreply.github.com>
2019-01-10 20:22:31 +0800

    Add files via upload

---
+++ src/components/pages/CustomerOrders.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <loading :active.sync="isLoading"></loading>
+        <div class="row mt-4">
+        <div class="col-md-4 mb-4"  v-for="item in products" :key="item.id">
+            <div class="card border-0 shadow-sm">
+                <div style="height: 150px; background-size: cover; background-position: center">
+                </div>
+                <div class="card-body">
+                <span class="badge badge-secondary float-right ml-2">分類7777</span>
+                <h5 class="card-title">
+                    <a href="#" class="text-dark">標題</a>
+                </h5>
+                <p class="card-text">內容</p>
+                <div class="d-flex justify-content-between align-items-baseline">
+                    <!-- <div class="h5">2,800 元</div> -->
+                    <del class="h6">原價 2,800 元</del>
+                    <div class="h5">現在只要 1,400 元</div>
+                </div>
+                </div>
+                <div class="card-footer d-flex">
+                <button type="button" class="btn btn-outline-secondary btn-sm">
+                    <i class="fas fa-spinner fa-spin"></i>
+                    查看更多
+                </button>
+                <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
+                    <i class="fas fa-spinner fa-spin"></i>
+                    加到購物車
+                </button>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+data(){
+        return{
+            products:[],
+            isLoading:false,
+        };
+    },
+    methods: {
+        getProducts(){
+        const vm = this;    
+        const api = `${process.env.APIPATH}api/${process.env.CUSTOMPATH}/products?page=${page}`;     
+        vm.isLoading=true;
+        this.$http.get(api).then((response) => {    
+        vm.products=response.data.products;
+        console.log(response); 
+        vm.isLoading=false;
+            });
+        },
+    },
+    created() {
+        this.getProducts();
+        
+    },
+};
